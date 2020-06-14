d48d60b20152a6339bcf77a478c939049e47d6fb
ArturPupchenko <pupchenkoartur@gmail.com>
2020-06-14 17:51:40 +0300

    first

---
+++ data/src/main/java/org/example/store/data/Entity/Hello.java
@@ -0,0 +1,7 @@
+package org.example.store.data.Entity;
+
+public class Hello {
+    public void sayHello(){
+        System.out.println("Hello World!!");
+    }
+}
---
+++ webtool/src/main/java/org/example/store/webtool/Main.java
@@ -0,0 +1,10 @@
+package org.example.store.webtool;
+
+import org.example.store.data.Entity.Hello;
+
+public class Main {
+    public static void main(String[] args) {
+    Hello hello = new Hello();
+    hello.sayHello();
+    }
+}
